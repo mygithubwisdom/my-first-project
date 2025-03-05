51d2729c27688a34c17f41988475937bb0968915
mygithubwisdom <hovafella@yahoo.com>
2025-03-06 00:44:58 +0100

    added Week3 to the project

---
+++ Week3/M4ACEdemo.js
@@ -0,0 +1,11 @@
+//This is my first Java script code!
+console.log('Hello World');
+function addition(x, y) {
+    return x + y; 
+}
+console.log(addition(20, 20));
+// Using a for loop to print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
